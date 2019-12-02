bc08a96b6a8a577202e0db4f1f6a15fc80401b21
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-02 13:24:55 -0700

    finished bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,6 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
-
+# when I tried to do that same thing in JS Math.pow(2, 65536) I got => Infinity
 # YOUR CODE HERE
+bignum = 2**65536
+print(bignum)
