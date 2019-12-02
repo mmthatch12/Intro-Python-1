985a64d6350580ac57df5831d797edc16c3797f1
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-02 15:51:04 -0700

    finished lists

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,29 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
-print(x)
+x.append(4)
+print('#1', x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+x = x + y
+print('#2', x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print(x)
+x.remove(8)
+print('#3', x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x)
+# print('#4', x)
+x.insert(5, 99)
+print('#4', x)
 
 # Print the length of list x
 # YOUR CODE HERE 
-
+print("#5", len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for num in x:
+    print("#6", [num*1000])
